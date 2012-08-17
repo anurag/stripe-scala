4c0c2c5081fc3ea719b2dbabb8f6bd738f8b365b
Siddarth Chandrasekaran <chandrasekaran.siddarth@gmail.com>
2012-08-16 17:39:43 -0700

    Bump version. Update readme.

--- src/main/scala/com/stripe/Stripe.scala
+++ src/main/scala/com/stripe/Stripe.scala
@@ -29,7 +29,7 @@ case class AuthenticationException(msg: String) extends StripeException(msg)
 
 abstract class APIResource {
   val ApiBase = "https://api.stripe.com/v1"
-  val BindingsVersion = "1.1.1"
+  val BindingsVersion = "1.1.2"
   val CharSet = "UTF-8"
 
   //lift-json format initialization
